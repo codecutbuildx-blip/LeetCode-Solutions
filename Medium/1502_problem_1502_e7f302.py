c30bc2c8f722dc42a1ed2e652a21e81ab3bf1259
codecutbuildx-blip <code.cut.build.x@gmail.com>
2026-02-11 22:01:09 +0530

    Solve: 1502. Problem 1502 (Medium)

---
+++ Medium/1502_problem_1502_e7f302.py
@@ -0,0 +1,14 @@
+# 1502. LeetCode Problem
+# LeetCode Link: https://leetcode.com/problems/
+
+class Solution:
+    def solve(self):
+        """
+        LeetCode Problem #1502
+        
+        AI generation temporarily unavailable.
+        Visit LeetCode to solve this problem manually.
+        """
+        pass
+
+# Solved: 2026-02-11
