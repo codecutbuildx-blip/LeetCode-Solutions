5879d105da8fe9a6269e64ab57a1e288f5408e87
codecutbuildx-blip <code.cut.build.x@gmail.com>
2026-02-11 23:16:28 +0530

    Solve: 105. Problem 105 (Medium)

---
+++ Medium/105_problem_105_95a8a8.py
@@ -0,0 +1,14 @@
+# 105. LeetCode Problem
+# LeetCode Link: https://leetcode.com/problems/
+
+class Solution:
+    def solve(self):
+        """
+        LeetCode Problem #105
+        
+        AI generation temporarily unavailable.
+        Visit LeetCode to solve this problem manually.
+        """
+        pass
+
+# Solved: 2026-02-11
